023313dcd2f7018812ce7352b308075a52154bec
Juriy Panasevich <elfelrandcor@gmail.com>
2015-10-10 11:11:08 +0300

    Initial Commit

---
+++ PasswordKeeperAppTests/PasswordKeeperAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  PasswordKeeperAppTests.swift
+//  PasswordKeeperAppTests
+//
+//  Created by Juriy Panasevich on 10.10.15.
+//  Copyright (c) 2015 Juriy Panasevich. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class PasswordKeeperAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
